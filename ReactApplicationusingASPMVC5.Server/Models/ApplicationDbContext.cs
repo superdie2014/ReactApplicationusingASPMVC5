28c31172f63df96af31c8f35ee0922062d928184
superdie2014 <tantran670@gmail.com>
2024-03-15 09:46:15 +0700

    v0.1

--- ReactApplicationusingASPMVC5.Server/Models/ApplicationDbContext.cs
+++ ReactApplicationusingASPMVC5.Server/Models/ApplicationDbContext.cs
@@ -4,7 +4,6 @@ namespace ReactApplicationusingASPMVC5.Server.Models
 {
     public class ApplicationDbContext:DbContext
     {
-        public DbSet<Customer> Customers { get; set; }
         public DbSet<Student> Students { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
         {
